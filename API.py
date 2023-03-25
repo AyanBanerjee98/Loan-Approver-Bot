3e4b66b17e262418f85283dcbbc987ef7978968a
ayanpro147 <122252933+ayanpro147@users.noreply.github.com>
2023-03-25 10:42:16 +0530

    Add files via upload

---
+++ API.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pickle 
+
+loan_model = pickle.load(open('loan_model.pkl', 'rb'))
+
+st.header("Loan Approver Bot")
+
+g = st.slider("Enter the gender (for male 1 and for female 0)",0,1)
+m = st.slider("Enter marital status (for married 1 and for unmarried 0)",0,1)
+d = st.number_input("Enter the number of dependents (for 3+ write 3 only)")
+e = st.slider("Enter the educational qualification (for non-graduates press 1 and graduates 0)",0,1)
+se = st.slider("Enter employment status (1 for employed 0 for un-employed)",0,1)
+ai = st.number_input("Enter applicant's income in thousand rupees")
+ci = st.number_input("Enter co-applicant's income in thousand rupees")
+l = st.number_input("Enter loan amount (Total amount / 1000)")
+t = st.number_input("Enter loan term in months")
+h = st.slider("Enter credit history (1 for yes and 0 for no)",0,1)
+a = st.slider("Enter property area (0 for rural, 1 for semi urban and 2 for urban)",0,1,2)
+
+if st.button("Predict"):
+    loan_status = loan_model.predict([[g,m,d,e,se,ai,ci,l,t,h,a]])
+    if (loan_status[0] == 1):
+        st.header("Applicant is eligible for loan")
+    else:
+        st.header("Not eligible")
